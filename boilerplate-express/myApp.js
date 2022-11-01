6a0f383e02b31360e348f6140831fd5de2149a75
Nathan <nslee333@gmail.com>
2022-11-01 02:26:24 -0700

    Added some api endpoints.

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,7 +1,17 @@
 let express = require('express');
 let app = express();
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+    res.json({
+        message: "Hello json"
+    });
+});
 
 
 
